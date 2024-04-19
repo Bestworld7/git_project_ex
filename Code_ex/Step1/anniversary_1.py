0f8df8859fd75f330b30bf7b1229128dd1620ccc
bestworld7 <ppbh@naver.com>
2024-04-20 00:47:54 +0900

    git test

--- Code_ex/Step1/anniversary_1.py
+++ Code_ex/Step1/anniversary_1.py
@@ -4,3 +4,5 @@ now = datetime.now()
 wedding_day = datetime(2012,3,1)
 wedding_das = now - wedding_day
 print(f"우리 결혼하지 + {wedding_das} 일째")
+roa_birthday = datetime(1976,5,4)
+print(f"나의 살아온 날은 {now - roa_birthday} 입니다")
